187b8178c5be81ccf69211e1d89201842fd5a6b5
Svegin <svegin@Svegins-Mini.attlocal.net>
2017-01-28 16:59:47 -0600

    finding wisdom

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 // function assembles html element
 function assembleSpan() {
-      const prBook = document.createElement('span');
-      prBook.className='sticker soldout_ribbon_right pos_right';
-      const txt_prBook = document.createTextNode('Печатное издание');
-      prBook.append(txt_prBook);
-      return prBook;
+    const prBook = document.createElement('span');
+    // prBook.className = 'sticker soldout_ribbon_right pos_right';
+    prBook.className = 'ribbon new';
+    // const txt_prBook = document.createTextNode('Печатное издание');
+    // prBook.append(txt_prBook);
+    return prBook;
 }
 // get className what we need
 const books = document.querySelectorAll('li.pa_varient_of_book-printed_book, div.pa_varient_of_book-printed_book .one-second.product_image_wrapper');
-console.log('trashItem',books);
+// console.log('trashItem', books);
 
 // insert html element for each className
 for (var i = 0; i < books.length; i++) {
-  const spanString = assembleSpan();
-  books[i].appendChild(spanString);
+    const spanString = assembleSpan();
+    books[i].appendChild(spanString);
 }
+
+// const newBook = document.querySelectorAll('li.post-1531');
+//
+// console.log('new', newBook);
+{/* <span class="ribbon new"></span> */}
